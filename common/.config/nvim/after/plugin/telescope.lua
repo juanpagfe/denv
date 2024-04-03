786c4ae1bcbff794e8b8b4c1dd895f857a5ac2ca
Juan Pablo Garcia <jpgarcia@multiplied.com>
2024-04-03 10:48:58 -0500

    Update telescope config

--- common/.config/nvim/after/plugin/telescope.lua
+++ common/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,11 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+
+vim.keymap.set('n', '<leader>ph', function()
+    builtin.find_files({ hidden=true })
+end)
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({ hidden=false })
+end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
