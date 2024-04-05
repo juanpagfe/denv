804cf7595ba242c50807167ac1a4d779fa69bfe7
Juan Pablo Garcia <jpgarcia@multiplied.com>
2024-04-05 07:11:04 -0500

    Install gitsigns

--- common/.config/nvim/lua/juanpagfe/packer.lua
+++ common/.config/nvim/lua/juanpagfe/packer.lua
@@ -24,6 +24,7 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use('lewis6991/gitsigns.nvim')
 
 
     use {
