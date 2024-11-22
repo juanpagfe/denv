572497ed8020bcda6bfd2139f243222ede252148
Juan Pablo Garcia <juanpagfe@gmail.com>
2024-11-22 12:12:06 -0500

    Updates packer and i3

--- common/.config/nvim/lua/juanpagfe/packer.lua
+++ common/.config/nvim/lua/juanpagfe/packer.lua
@@ -38,5 +38,4 @@ return require('packer').startup(function(use)
         "saadparwaiz1/cmp_luasnip",
         "rafamadriz/friendly-snippets"
     }
-    use { "kylechui/nvim-surround", tag = "*" }
 end)
