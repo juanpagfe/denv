84c6008c08830cdbb50cf1c3745cad10797d1986
Juan Pablo Garcia <jpgarcia@multiplied.com>
2024-12-19 08:55:39 -0500

    Updates executables

--- env/0global.sh
+++ env/0global.sh
@@ -21,14 +21,13 @@ export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:/home/jpgarcia/.cargo/bin
 export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/home/jpgarcia/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
 
-# Bash file stored in ~/.local/bin/
-set_machine
+export MACHINE=$(get_machine)
 
 #Starts an http server on the current directory (Default port: 8000)
 alias www='python3 -m http.server'
 
 #Prints current opened ports
-if [ "$MACHINE" = "Mac" ]; then
+if [[ "$MACHINE" = "Mac" ]]; then
     alias ports="netstat -lant | grep -E -i -w 'LISTEN|udp4|udp6'"
 else
     alias ports='netstat -tulanp'
