eb12de7fd81a264c281177cf19a1c4e5183b58fe
Juan Pablo Garcia <juanpagfe@gmail.com>
2024-12-23 09:15:43 -0500

    Fixes treesitter and creates c_init

--- common/.config/nvim/after/plugin/treesitter.lua
+++ common/.config/nvim/after/plugin/treesitter.lua
@@ -1,11 +1,11 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp",
+  ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "c_sharp",
   "javascript", "typescript", "python", "rust", "html", "go", "toml",
   "markdown", "json", "yaml", "bash" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
