cf153304d60c2bf67c372eccee2bf3bd8788f514
Juan Pablo Garcia <juanpagfe@gmail.com>
2024-12-28 20:58:21 -0500

    Transform to alias

--- env/embedded.sh
+++ env/embedded.sh
@@ -8,13 +8,5 @@
 export ESP_IDF_PATH=/opt/esp-idf
 export DEF_ESP_CHIP="esp32c3"
 export DEF_ESP_PORT=/dev/ttyACM0
+alias idf="idf.py"
 
-function idf() {
-    if [ -z "$IDF_PATH" ]; then
-        echo "You need to load esp-idf framework before using this program."
-        echo "Run:"
-        echo "  source $ESP_IDF_PATH/export.sh"
-        return 1
-    fi
-    idf.py $@
-}
