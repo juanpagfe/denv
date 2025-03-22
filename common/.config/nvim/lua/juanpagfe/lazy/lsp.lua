c6df3ddbc174698682b9a434bd2c34c6d0ea5cdf
Juan Pablo Garcia <juanpagfe@gmail.com>
2025-03-22 18:26:19 -0500

    Clean-up lsp file

--- common/.config/nvim/lua/juanpagfe/lazy/lsp.lua
+++ common/.config/nvim/lua/juanpagfe/lazy/lsp.lua
@@ -4,173 +4,69 @@ return {
         "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "windwp/nvim-ts-autotag"
     },
     config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-            }
-        })
+        local cmp = require('cmp')
+        local luasnip = require("luasnip")
+        require("mason").setup()
+        require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "solargraph" } })
+        require("nvim-ts-autotag").setup()
+        require("luasnip.loaders.from_vscode").lazy_load()
+
         local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        require('nvim-ts-autotag').setup()
-
-        require("mason").setup()
-
-        -- Update LSP capabilities for completion
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            lspconfig.util.default_config.capabilities,
-            cmp_nvim_lsp.default_capabilities()
-        )
-
-        require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "solargraph" },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-                ["lua_ls"] = function()
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
-
-        -- Specific LSP configurations
-        lspconfig.pylyzer.setup({
-            settings = {
-                python = {
-                    checkOnType = false,
-                    diagnostics = true,
-                },
-            },
-        })
-
-        lspconfig.ts_sl.setup({})
-        lspconfig.emmet_ls.setup({
-            filetypes = { "html", "javascriptreact", "typescriptreact" }
-        })
-
-        local function get_python_path()
-            local venv = vim.fn.getenv("VIRTUAL_ENV")
-            if venv and venv ~= vim.NIL then
-                return venv .. "/bin/python3"
+        local servers = { "ts_ls", "html", "cssls", "jsonls", "clangd", "pyright", "eslint", "groovyls", "rust_analyzer" }
+        for _, server in ipairs(servers) do
+            if server == "groovyls" then
+                lspconfig[server].setup({
+                    capabilities = capabilities,
+                    filetypes = { "groovy", "Jenkinsfile" }
+                })
             else
-                return vim.fn.exepath("python3")
+                lspconfig[server].setup({ capabilities = capabilities })
             end
         end
-
-        lspconfig.pyright.setup({
-            settings = {
-                python = {
-                    pythonPath = get_python_path(),
-                }
-            }
-        })
-
-        lspconfig.clangd.setup({
-            on_attach = function(client, bufnr)
-                if client.server_capabilities.documentFormattingProvider then
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                        buffer = bufnr,
-                        callback = function() vim.lsp.buf.format() end,
-                    })
-                end
-            end,
-        })
-
-        -- TypeScript/JavaScript (TSX/JSX)
-        lspconfig.ts_ls.setup {}
-
-        -- HTML
-        lspconfig.html.setup {}
-
-        -- CSS
-        lspconfig.cssls.setup {}
-
-        -- JSON
-        lspconfig.jsonls.setup {}
-
-        lspconfig.groovyls.setup({
-            cmd = { "groovy-language-server" },
-            filetypes = { "groovy", "Jenkinsfile" },
-            root_dir = lspconfig.util.root_pattern("Jenkinsfile", ".git"),
-            on_attach = function()
-                vim.diagnostic.config({
-                    virtual_text = { prefix = "● " },
-                    severity_sort = true,
-                })
-            end,
-        })
-
-        lspconfig.rust_analyzer.setup({
-            settings = {
-                ["rust-analyzer"] = {
-                    server = { path = "~/.cargo/bin/rust-analyzer" },
-                    cargo = { allFeatures = true },
-                }
-            }
-        })
 
         lspconfig.lua_ls.setup({
             settings = {
                 Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
+                    diagnostics = { globals = { "vim" } },
                     workspace = {
-                        library = {
-                            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                            [vim.fn.stdpath "config" .. "/lua"] = true,
-                        },
-                    },
-                },
+                        library = { [vim.fn.expand("$VIMRUNTIME/lua")] = true }
+                    }
+                }
             }
         })
 
+        lspconfig.emmet_ls.setup({
+            capabilities = capabilities,
+            filetypes = { "html", "javascriptreact", "typescriptreact" }
+        })
+
         vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
                 local opts = { buffer = ev.buf }
-                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set("n", "<space>wl", function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-                vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                vim.keymap.set("n", "<space>f", function()
-                    vim.lsp.buf.format { async = true }
-                end, opts)
-            end,
+                local keymap = vim.keymap.set
+                keymap("n", "gD", vim.lsp.buf.declaration, opts)
+                keymap("n", "gd", vim.lsp.buf.definition, opts)
+                keymap("n", "K", vim.lsp.buf.hover, opts)
+                keymap("n", "gi", vim.lsp.buf.implementation, opts)
+                keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+                keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                keymap("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+                keymap("n", "<space>D", vim.lsp.buf.type_definition, opts)
+                keymap("n", "<space>rn", vim.lsp.buf.rename, opts)
+                keymap({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+                keymap("n", "gr", vim.lsp.buf.references, opts)
+                keymap("n", "<space>f", function() vim.lsp.buf.format { async = true } end, opts)
+            end
         })
 
         vim.diagnostic.config({
@@ -183,19 +79,6 @@ return {
                 prefix = "",
             },
         })
-
-        lspconfig.eslint.setup({
-            on_attach = function(client, bufnr)
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    buffer = bufnr,
-                    command = "EslintFixAll",
-                })
-            end,
-        })
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
-
-        require("luasnip.loaders.from_vscode").lazy_load()
 
         local has_words_before = function()
             local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
@@ -249,6 +132,6 @@ return {
                     { name = 'buffer' },
                 }),
         })
+
     end,
 }
-
