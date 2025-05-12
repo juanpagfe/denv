8e922c17e15b505929b05774b1dc8a74af944e1a
Juan Pablo Garcia <juanpagfe@gmail.com>
2025-05-12 12:24:11 -0500

    Update

--- env/0global.sh
+++ env/0global.sh
@@ -96,7 +96,7 @@ function rt() {
 function sethome() {
 #    if pgrep -x "i3" > /dev/null
 #    then
-    xrandr --output eDP-1 --mode 3072x1920 --scale 0.7x0.7 --pos 1920x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    xrandr --output eDP-1-1 --mode 3072x1920 --scale 0.7x0.7 --pos 1920x0 --rotate normal --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 #    else
 #        echo "i3 is not running"
 #    fi
@@ -106,7 +106,7 @@ function sethome() {
 function setalone() {
 #    if pgrep -x "i3" > /dev/null
 #    then
-    xrandr --output eDP-1 --mode 3072x1920 --scale 0.7x0.7 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off
+    xrandr --output eDP-1-1 --mode 3072x1920 --scale 0.7x0.7 --pos 0x0 --rotate normal --output HDMI-1-1 --off --output DP-1 --off
 #    else
 #        echo "i3 is not running"
 #    fi
--- linux/.config/i3/startup.sh
+++ linux/.config/i3/startup.sh
@@ -3,46 +3,13 @@
 feh --bg-scale ~/Pictures/background.jpg
 
 if xrandr --query | grep -qE "HDMI.* connected"; then
-    xrandr --output eDP-1 --mode 3072x1920 --scale 0.7x0.7 --pos 1920x0 --rotate normal
-    xrandr --output HDMI-1 --primary --mode 1920x1080 --scale 1x1 --pos 0x0 --rotate normal
-    declare -A screen_workspaces=(
-        ["DP-1"]="10"
-        ["HDMI-1"]="1 2"
-    )
-    declare -A workspace_apps=(
-        ["1"]="ghostty"
-        ["2"]="google-chrome"
-    )
+    xrandr --output eDP-1-1 --mode 3072x1920 --scale 0.7x0.7 --pos 1920x0 --rotate normal
+    xrandr --output HDMI-1-1 --primary --mode 1920x1080 --scale 1x1 --pos 0x0 --rotate normal
+    i3-msg "workspace 1; move workspace to output HDMI-1-1"
+    i3-msg "workspace 2; move workspace to output HDMI-1-1"
+    i3-msg "workspace 10; move workspace to output eDP-1-1"
 else
-    xrandr --output eDP-1 --mode 3072x1920 --scale 0.7x0.7 --pos 0x0 --rotate normal --output HDMI-1 --off
-    declare -A screen_workspaces=()
-    declare -A workspace_apps=(
-        ["1"]="ghostty"
-        ["2"]="google-chrome"
-    )
+    xrandr --output eDP-1-1 --mode 3072x1920 --scale 0.7x0.7 --pos 0x0 --rotate normal --output HDMI-1-1 --off
+    i3-msg "workspace 1; move workspace to output eDP-1-1"
+    i3-msg "workspace 2; move workspace to output eDP-1-1"
 fi
-
-# Assign each workspace to the appropriate screen
-for screen in "${!screen_workspaces[@]}"; do
-    for workspace in ${screen_workspaces[$screen]}; do
-        # Move the workspace to the specified screen
-        i3-msg workspace $workspace
-        i3-msg move workspace to output $screen
-    done
-done
-
-for workspace in "${!workspace_apps[@]}"; do
-    # Switch to the workspace
-    i3-msg workspace $workspace
-    
-    # Launch the application
-    ${workspace_apps[$workspace]} &
-    
-    # Add a brief delay to ensure the application launches in the correct workspace
-    sleep 0.5
-done
-
-set +e
-#setxkbmap -layout us -model pc105 -variant altgr-intl -option compose:ralt,terminate:ctrl_alt_bksp
-#xinput set-button-map 12 1 2 3 4 5 6 7 0 0 10 11 12
-set -e
