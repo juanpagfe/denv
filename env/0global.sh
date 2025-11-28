c4ade2bf045fec4e615c9255340b2f10241371ad
Juan Pablo Garcia <juanpagfe@gmail.com>
2025-11-28 07:49:22 -0500

    Updates

--- env/0global.sh
+++ env/0global.sh
@@ -79,6 +79,7 @@ alias tmux="$tmux -2"
 alias vim='nvim'
 alias prime-run='env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia'
 alias nano='nvim'
+alias aptupgrade='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
 
 ###############################################################################################
 #                                                                                             #
