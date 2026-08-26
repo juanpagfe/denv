6e6fef382034bfe94547fc6dcaf9fb54f1998ad9
Juan Pablo Garcia <jpgarcia@multiplied.com>
2026-08-26 11:17:55 -0500

    wezterm

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -6,7 +6,6 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices.
 config.enable_tab_bar = false
-config.default_gui_startup_args = { "start", "--maximize" }
 
 config.window_padding = {
   left = 5,
